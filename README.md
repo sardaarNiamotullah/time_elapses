b1858cbfe9cc1a75bee3cd409a1d8ec6634c8ed6
sardaarNiamotullah <sardaar.niamotullah@gmail.com>
2024-08-09 22:42:43 +0600

    second commit

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Time Elapses Web App
 
 A web application that calculates the elapsed time since a given date. Ideal for tracking the time passed since significant events or milestones.
+
+### to see the site, [**CLICK HERE!**](https://sardaarniamotullah.github.io/time_elapses/)
--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-const startDate = new Date("2024-08-05T00:00:00");
+const startDate = new Date("2024-08-10T00:00:00");
 
 function pad(num, size) {
   let s = "0".repeat(size) + num;
@@ -47,3 +47,44 @@ document.addEventListener("DOMContentLoaded", () => {
   updateStopwatch();
   setInterval(updateStopwatch, 1000);
 });
+
+// animation code...
+
+function updateLineWidth() {
+  const now = new Date();
+
+  const lineSecond = document.querySelector(".lines_container_line_second");
+  const lineMinute = document.querySelector(".lines_container_line_minute");
+  const lineHour = document.querySelector(".lines_container_line_hour");
+  const lineDay = document.querySelector(".lines_container_line_day");
+
+  const milliseconds = now.getMilliseconds();
+  const second = now.getSeconds();
+  const minute = now.getMinutes();
+  const hour = now.getHours();
+
+  const secondPercent = (milliseconds / 1000) * 100;
+  const minutePercent = ((second + milliseconds / 1000) / 60) * 100;
+  const hourPercent = ((minute + second / 60) / 60) * 100;
+  const dayPercent = ((hour + minute / 60) / 24) * 100;
+
+  // lineSecond.style.width = `${(secondPercent* 80) / 100}px`;
+  // lineMinute.style.width = `${(minutePercent* 80) / 100}px`;
+  // lineHour.style.width = `${(hourPercent* 80) / 100}px`;
+  // lineDay.style.width = `${(dayPercent* 80) / 100}px`;
+
+  lineSecond.style.width = `${secondPercent}%`;
+  lineMinute.style.width = `${minutePercent}%`;
+  lineHour.style.width = `${hourPercent}%`;
+  lineDay.style.width = `${dayPercent}%`;
+
+  // lineSecond.style.backgroundColor = `rgb(${minutePercent * 2.6}, ${secondPercent * 2.6}, ${hourPercent * 2.6})`;
+  // lineMinute.style.backgroundColor = `rgb(${minutePercent * 2.6}, ${hourPercent * 2.6}, ${secondPercent * 2.6})`;
+  // lineHour.style.backgroundColor = `rgb(${hourPercent * 2.6}, ${minutePercent * 2.6}, ${secondPercent * 2.6})`;
+  // lineDay.style.backgroundColor = `rgb(${hourPercent * 2.6}, ${secondPercent * 2.6}, ${minutePercent * 2.6})`;
+}
+function startClock() {
+  updateLineWidth(); // Initial update
+  setInterval(updateLineWidth, 1); // Update every Milli second
+}
+startClock();
